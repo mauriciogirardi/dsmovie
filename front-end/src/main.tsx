bfea9f1f0cb6e6f3f0a37ba293652c3066e6b505
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-07 14:27:04 -0300

    config chakra-ui

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { dark } from './styles/theme/dark';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={dark}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
