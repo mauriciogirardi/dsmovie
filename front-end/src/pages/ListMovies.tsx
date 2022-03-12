ceeda52d85077e8e741ef5747fa62209f85b9abb
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-12 15:24:34 -0300

    create list movies

--- front-end/src/pages/ListMovies.tsx
+++ front-end/src/pages/ListMovies.tsx
@@ -1,14 +1,28 @@
 import { Flex, SimpleGrid } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MovieCard } from '../components/MovieCard';
 import { Pagination } from '../components/Pagination';
+import { SkeletonMovieList } from '../components/SkeletonMovieList';
+import { useMovies } from '../hooks/useMovies';
 
 export default function ListMovies() {
   const [page, setPage] = useState(1);
+
+  const { fetchMovies, movies, fetching } = useMovies();
+
+  useEffect(() => {
+    fetchMovies({
+      page: page - 1,
+      size: 9,
+      showAll: true,
+      sort: ['title', 'asc'],
+    });
+  }, [fetchMovies, page]);
+
   return (
     <Flex
       flexDir='column'
-      h={['100%', '87vh']}
+      h={'100%'}
       align='center'
       w='100%'
     >
@@ -18,20 +32,25 @@ export default function ListMovies() {
           spacingX='40px'
           spacingY='20px'
         >
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
+          {fetching || !movies?.content.length ? (
+            <SkeletonMovieList />
+          ) : (
+            <>
+              {movies.content.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </>
+          )}
         </SimpleGrid>
       </Flex>
 
-      <Pagination
-        totalCountRegister={5}
-        currentPage={2}
-        onPageChange={setPage}
-      />
+      <Flex justify='flex-end' w='87%'>
+        <Pagination
+          currentPage={movies?.number + 1}
+          onPageChange={setPage}
+          lastPage={movies?.totalPages || 0}
+        />
+      </Flex>
     </Flex>
   );
 }
